f9fa4604da9cdf30236d7a83731c3ad82ae061bb
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-07-31 22:54:10 +0430

    react-icons package added

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,15 +1,17 @@
-import {CiSearch} from "react/icons/ci"
+import { CiSearch } from "react-icons/ci";
 
 const Notes = () => {
   return (
     <section>
       <header className="notes__header">
         <h2>My Notes</h2>
-        <input type="text" autoFocus placeholder='Keyword...' />
-        <button className="btn"><CiSearch/></button>
+        <input type="text" autoFocus placeholder="Keyword..." />
+        <button className="btn">
+          <CiSearch />
+        </button>
       </header>
     </section>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
