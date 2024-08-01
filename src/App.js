cf67ce6292f54647f3a89cdcbb86214c690004b0
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-01 11:54:28 +0430

    main element added for wrapping the entire application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Notes from './pages/Notes';
-import CreateNote from './pages/CreateNote';
-import EditNote from './pages/EditNote';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import CreateNote from "./pages/CreateNote";
+import EditNote from "./pages/EditNote";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Notes />}/>
-        <Route path='create-note' element={<CreateNote />}/>
-        <Route path='/edit-note/:id' element={<EditNote />}/>
-      </Routes>
-    </BrowserRouter>
+    <main id="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="create-note" element={<CreateNote />} />
+          <Route path="/edit-note/:id" element={<EditNote />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
