48d13f729d4b3ef9bba45672ca0f5fe8d04ced35
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-01 11:31:06 +0430

    NoteItem added in Notes page

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,15 +1,25 @@
 import { CiSearch } from "react-icons/ci";
+import { BsPlusLg } from "react-icons/bs";
+import dummyNotes from "../dummy_notes";
+import { Link } from "react-router-dom";
+import NoteItem from "../components/NoteItem";
 
 const Notes = () => {
   return (
     <section>
       <header className="notes__header">
         <h2>My Notes</h2>
-        <input type="text" autoFocus placeholder="Keyword..." />
+        {/* <input type="text" autoFocus placeholder="Keyword..." /> */}
         <button className="btn">
           <CiSearch />
         </button>
       </header>
+      <div className="notes__container">
+        {
+          dummyNotes.map(note => <NoteItem key={note.id} note={note} />)
+        }
+      </div>
+      <Link className="btn add__btn"><BsPlusLg /></Link>
     </section>
   );
 };
