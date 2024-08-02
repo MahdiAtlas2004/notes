cb71c724a142bcbd7050ff76f560978a769376f0
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-02 10:08:11 +0430

    handleSubmit function and states added to CreateNote.jsx file for creating new notes

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,18 +1,52 @@
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import useCreateDate from "../hooks/useCreateDate";
+import { useNavigate } from "react-router-dom";
 
-const CreateNote = () => {
+const CreateNote = ({setNotes}) => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      const note = { id: uuidv4(), title, details, date };
+      // Adding new note to notes array.
+      setNotes(prevNotes => [note, ...prevNotes]);
+
+      // Redirect user to home page.
+      navigate("/");
+    }
+  };
+
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className="btn">
           <IoIosArrowBack />
         </Link>
-        <button className="btn lg primary">Save</button>
+        <button className="btn lg primary" onClick={handleSubmit}>
+          Save
+        </button>
       </header>
-      <form className="create-note__form">
-        <input type="text" placeholder="Title" autoFocus />
-        <textarea rows="28" placeholder="Note details..."></textarea>
+      <form className="create-note__form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          autoFocus
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          rows="28"
+          placeholder="Note details..."
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        ></textarea>
       </form>
     </section>
   );
