d88c776d5c5b8eeddc8e3ceccb0db068f7cd4c61
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-03 14:33:46 +0430

    Editing functionality added to EditNote page.

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,22 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { useState } from "react";
+import useCreateDate from "../hooks/useCreateDate";
 
-const EditNote = () => {
+const EditNote = ({ notes, setNotes }) => {
+  const { id } = useParams();
+  const date = useCreateDate();
+  const note = notes.find((item) => item.id === id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      const newNote = { ...note, title, details, date };
+      const newNotes = notes.map((item) => {
+        if (item.id === id) {
+          item = newNote;
+        }
+        return item;
+      });
+      setNotes(newNotes);
+    }
+    // Redirect user to home page.
+    navigate("/");
+  };
+
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className="btn">
           <IoIosArrowBack />
         </Link>
-        <button className="btn lg primary">Save</button>
+        <button className="btn lg primary" onClick={handleSubmit}>
+          Save
+        </button>
         <button className="btn lg danger">
           <RiDeleteBin6Line />
         </button>
       </header>
-      <form className="create-note__form">
-        <input type="text" placeholder="Title" autoFocus />
-        <textarea rows="28" placeholder="Note details..."></textarea>
+      <form className="create-note__form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          autoFocus
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          rows="28"
+          placeholder="Note details..."
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        ></textarea>
       </form>
     </section>
   );
