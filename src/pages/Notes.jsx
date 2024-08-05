0c2d39a2a62866e88c10ce7d3d588079eaa821ad
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-05 07:31:45 +0430

    Search input and functionality added to notes page.

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,24 +1,62 @@
 import { CiSearch } from "react-icons/ci";
+import { MdClose } from "react-icons/md";
 import { BsPlusLg } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import NoteItem from "../components/NoteItem";
+import { useEffect, useState } from "react";
 
-const Notes = ({notes}) => {
+const Notes = ({ notes }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      notes.filter((note) => {
+        if (note.title.toLowerCase().match(text.toLowerCase())) {
+          return note;
+        }
+      })
+    );
+  };
+
+  useEffect(handleSearch, [text]);
+
   return (
     <section>
       <header className="notes__header">
-        <h2>My Notes</h2>
-        {/* <input type="text" autoFocus placeholder="Keyword..." /> */}
-        <button className="btn">
-          <CiSearch />
+        {!showSearch ? (
+          <h2>My Notes</h2>
+        ) : (
+          <input
+            type="text"
+            autoFocus
+            placeholder="Keyword..."
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              handleSearch();
+            }}
+          />
+        )}
+        <button
+          className="btn"
+          onClick={() => setShowSearch((prevState) => !prevState)}
+        >
+          {!showSearch ? <CiSearch /> : <MdClose onClick={() => setText("")} />}
         </button>
       </header>
       <div className="notes__container">
-        {
-          notes.map(note => <NoteItem key={note.id} note={note} />)
-        }
+        {filteredNotes.length === 0 && (
+          <p className="empty__notes">No notes found.</p>
+        )}
+        {filteredNotes.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
       </div>
-      <Link className="btn add__btn" to="/create-note"><BsPlusLg /></Link>
+      <Link className="btn add__btn" to="/create-note">
+        <BsPlusLg />
+      </Link>
     </section>
   );
 };
